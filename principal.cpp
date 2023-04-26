a0c3d05e811d5c5b4a104fab8e80802cb416e74f
jadg13 <jaduran@doc.uca.edu.ni>
2023-04-26 15:59:45 -0600

    Ejemplo

---
+++ principal.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "MisArchivos\\funciones.cpp"
+#include "MisArchivos\\menu.cpp"
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << "\tMi proyecto - Prueba" << endl;
+    bienvenida();
+    despedida();
+
+    return 0;
+}
